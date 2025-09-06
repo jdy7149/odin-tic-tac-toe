d9ee75a27ac7f10e46c34eaef6b3c6504929a67b
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2025-09-07 02:18:15 +0900

    Add createUser

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function createGameboard() {
-    const row = 3;
-    const column = 3;
+    const rows = 3;
+    const columns = 3;
     const board = [];
 
-    for (let i = 0; i < row; i++) {
+    for (let i = 0; i < rows; i++) {
         board.push([]);
-        for (let j = 0; j < column; j++) {
+        for (let j = 0; j < columns; j++) {
             board[i].push(createCell());
         }
     }
@@ -13,6 +13,12 @@ function createGameboard() {
     const printBoard = console.log(board);
 
     const getBoard = () => board;
+
+    const placeToken = (row, column, token) => {
+
+    }
+
+
 
     return {printBoard, getBoard};
 }
@@ -23,8 +29,57 @@ function createCell() {
     const addToken = (token) => value = token;
 
     const getValue = () => value;
+
+    return {
+        addToken,
+        getValue
+    }
 }
 
-function createGameController(player1, player2) {
+function createUser(name, token) {
+    let score = 0;
+
+    const incrementScore = () => score++;
+
+    const getScore = () => score;
+
+    const getName = () => name;
+
+    const getToken = () => token;
+
+    return {
+        incrementScore,
+        getScore,
+        getName,
+        getToken
+    }
+} 
+
+function createGameController(name1 = 'Player One', name2 = 'Player Two') {
+    const board = createGameboard();
+    
+    const players = [
+        {
+            name: name1,
+            token: 'O'
+        },
+        {
+            name: name2,
+            token: 'X'
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = players[0] ? players[1] : players[0];
+    }
+
+    const getActivePlayer = () => activePlayer;
+
+    const getNewRoundText = () => {
+        board.printBoard();
+        return `${activePlayer.name}'s turn.`;
+    }
 
 }
